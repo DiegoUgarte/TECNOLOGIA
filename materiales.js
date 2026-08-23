d0fc7815dc1b015359d6f1555352336447ebf8b1
DiegoUgarte <diegougarte971@gmail.com>
2026-08-23 12:11:00 -0400

    primera version

---
+++ materiales.js
@@ -0,0 +1,30 @@
+const materiales = [
+
+    {
+        id: 1,
+        curso: "5",
+        categoria: "Python",
+        nombre: "Guía de Introducción a Python",
+        descripcion: "Material de apoyo para la primera clase.",
+        archivo: "materiales/5to/python/guia-python.pdf"
+    },
+
+    {
+        id: 2,
+        curso: "5",
+        categoria: "HTML",
+        nombre: "Ejercicios HTML",
+        descripcion: "Ejercicios prácticos de etiquetas HTML.",
+        archivo: "materiales/5to/html/ejercicios-html.pdf"
+    },
+
+    {
+        id: 3,
+        curso: "4",
+        categoria: "Tecnología Gráfica",
+        nombre: "Introducción a Tecnología Gráfica",
+        descripcion: "Material de estudio.",
+        archivo: "materiales/4to/tecnologia-grafica/introduccion.pdf"
+    }
+
+];
